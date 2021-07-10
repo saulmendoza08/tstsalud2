1119b46389fd3e48ff1cf9e0a28488d602b13170
unknown <gabrielfmendoza>
2021-07-10 10:47:05 -0300

    alpha 2

--- assets/modules/nav.js
+++ assets/modules/nav.js
@@ -2,9 +2,9 @@ const d = document;
 export default function navScroll(n){
     const $nav = d.getElementById(n)
     if(scrollY>15){
-        $nav.classList.remove("nav-azul");
+        $nav.classList.remove("nav-tras");
     }
     d.addEventListener('scroll',e=>{
-        (scrollY>15)?$nav.classList.remove("nav-azul"):$nav.classList.add("nav-azul");
+        (scrollY>15)?$nav.classList.remove("nav-tras"):$nav.classList.add("nav-tras");
     })
 }
